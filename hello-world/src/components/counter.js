443c115585a57387660eb8ba7f04aad821ae4b54
rahul1237 <rahulmahajan98645@gmail.com>
2021-03-14 06:45:54 +0530

    added

--- hello-world/src/components/counter.js
+++ hello-world/src/components/counter.js
@@ -11,22 +11,54 @@ class Counter  extends Component{
     }
 
     increase(){
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
+        // this.setState({
+        //     count: this.state.count + 1
+        // } , () => {
+        //     console.log('value-1: ',this.state.count);
+        // })
 
+        this.setState(prev => ({
+            count: prev.count + 1
+        }) , () => {
+            console.log('value-1: ',this.state.count);
+        })
+
+        console.log('value-2: ',this.state.count);
+    }
+    
+    increase2(){
+        this.increase()
+        this.increase()
+    }
     
     decrease(){
-        this.setState({
-            count: this.state.count - 1
+        // this.setState({
+        //     count: this.state.count - 1
+        // } , () => {
+        //     console.log('value-1: ',this.state.count);
+        // })
+
+        this.setState(prev => ({
+            count: prev.count - 1
+        }) , () => {
+            console.log('value-1: ',this.state.count);
         })
+
+        console.log('value-2: ',this.state.count);
+    }
+    
+    decrease2(){
+        this.decrease()
+        this.decrease()
     }
 
     reset(){
         this.setState({
             count: 0
+        } , () => {
+            console.log('value-1: ',this.state.count);
         })   
+        console.log('value-2: ',this.state.count);
     }
 
     render(){
@@ -35,7 +67,9 @@ class Counter  extends Component{
                 <h1>
                     count : {this.state.count}
                 </h1>
+                <button onClick = { () => this.increase2()  }  >increase the value twice!</button>
                 <button onClick = { () => this.increase()  }  >increase the value!</button>
+                <button onClick = { () => this.decrease2()  }  >decrease the value twice!</button>
                 <button onClick = { () => this.decrease()  }  >decrease the value!</button>
                 <button onClick = { () => this.reset()  }  >reset the value!</button>
             </div>
